3a16e6ac23c557ab3998cc720e96752a0336740c
ajain96 <anubhavjain2809@gmail.com>
2022-06-23 11:14:11 +0530

    file added

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using E_Commerce.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace E_Commerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        ECommerceDB1Context db;
+        public ProductController(ECommerceDB1Context _db)
+        {
+            db = _db;
+        }
+        [HttpGet]
+        public IEnumerable<TblProduct> GetProducts()
+        {
+            return db.TblProducts;
+        }
+    }
+}
---
+++ Interfaces/IJWTManagerRepository.cs
@@ -0,0 +1,9 @@
+﻿using E_Commerce.ViewModels;
+
+namespace E_Commerce.Interfaces
+{
+    public interface IJWTMangerRepository
+    {
+        Tokens Authenicate(LoginViewModel users, bool IsRegister);
+    }
+}
